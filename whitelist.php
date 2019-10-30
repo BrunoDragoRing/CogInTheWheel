c7c167cb7aa515cb6d12a0f141acf85f50dc9c7f
Bruno Drago <bruno.drago@ring.com>
2019-10-30 14:01:24 -0700

    whitelist file

---
+++ whitelist.php
@@ -0,0 +1,26 @@
+<?php
+
+$whitelist = array("device-groups-service",
+"ring-billing",
+"ring-api",
+"admin-panel",
+"clients-api",
+"status-reports-low-battery-notifier",
+"cvcore",
+"cvcore-message-filtering",
+"motion_verification_and_stop_service",
+"ring-media-server",
+"firmware-release-mgmt",
+"ring-masters-ruby",
+"ring-nginx-gw)",
+"status-reports-service",
+"ring-socket-service",
+"device-service",
+"notification-service-go",
+"snapshots-service",
+"ding-validator",
+"integrations-services-go",
+"status-report-reader",
+"ring-oauth-service");
+
+?>
