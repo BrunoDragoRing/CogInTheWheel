f3db2ae04137b5b0df5107cab8db1b510cf883ca
Bruno Drago <bruno.drago@ring.com>
2020-01-03 10:01:07 -0800

    whitelist.php

--- whitelist.php
+++ whitelist.php
@@ -1,28 +1,85 @@
 <?php
-$whitelist = array(
-"activity-service",
-"clients-api",
-"cvcore",
-"device-groups-service",
-"ding-validator",
-"eligibility-service",
-"integrations-services-go",
-"molfar",
-"motion_verification_and_stop_service",
-"news-aggregator",
-"notifications-settings-service",
-"packetloss-service",
-"recordings-manager",
-"ring-api",
-"ring-oauth-service",
-"ring-socket-service",
-"rms-transcoder",
-"snapshots-service",
-"stark",
-"stark-reference-service",
-"stark-sns-event-consumer",
-"status-reports-capabilities-consumer",
-"status-reports-low-battery-notifier",
-);
-?>
+require __DIR__ . '/vendor/autoload.php';
 
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Google Sheets API PHP Quickstart');
+    $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
+    $client->setAuthConfig('credentials.json');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+
+    // Load previously authorized token from a file, if it exists.
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    $tokenPath = 'token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
+
+
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Google_Service_Sheets($client);
+
+// Prints the names and majors of students in a sample spreadsheet:
+// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+$spreadsheetId = '1e7uRdc-ZvHeiK2-MoTyzk2s3H6b00uz50OUOeRyoBGM';
+$range = "'Fast line list'!A:B";
+$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+$values = $response->getValues();
+
+if (empty($values)) {
+    print "Error fetching whitelist.\n";
+	exit(1);
+} else {
+	$whitelist = array();
+    foreach ($values as $row) {
+        // Print columns A and E, which correspond to indices 0 and 4.
+        if ($row[1] == "fast lane") {
+			array_push($whitelist, $row[0]);
+		}
+    }
+}
+?>
